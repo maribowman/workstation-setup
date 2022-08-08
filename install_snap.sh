458300276e36056667ca1a43a96b35f2f8a59d9e
mari__ <bowman.marius@gmail.com>
2022-08-08 18:05:45 +0200

    add install snap apps

---
+++ install_snap.sh
@@ -0,0 +1,12 @@
+sudo apt update && sudo apt install -y snapd
+
+# TODO test me
+sudo snap refresh
+sudo snap install goland --classic
+sudo snap install helm --classic
+sudo snap install kubectl --classic
+sudo snap install postman
+sudo snap install keepassxc
+sudo snap install notion-snap
+sudo snap install discord
+sudo snap install teams

58599de28c8de8040d037b5e7c9b07125ea8c251
mari__ <bowman.marius@gmail.com>
2022-08-08 18:30:02 +0200

    add install resilio sync

---
+++ install_resilio.sh
@@ -0,0 +1,7 @@
+# https://www.resilio.com/blog/official-linux-packages-for-sync-now-available
+
+deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free
+wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
+
+sudo apt update && sudo apt install -y resilio-sync
+sudo systemctl enable resilio-sync

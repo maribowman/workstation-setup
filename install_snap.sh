da9ba0203b5e2d0cf5b2c4bf9a4439c636f096d0
mari__ <bowman.marius@gmail.com>
2022-08-10 17:26:03 +0200

    format file

--- install_snap.sh
+++ install_snap.sh
@@ -4,10 +4,6 @@ sudo snap refresh
 sudo snap install goland --classic
 sudo snap install helm --classic
 sudo snap install kubectl --classic
-sudo snap install postman
-sudo snap install keepassxc
-sudo snap install notion-snap
-sudo snap install discord
-sudo snap install teams
+sudo snap install postman keepassxc notion-snap discord teams
 
 #sudo snap install tradingview

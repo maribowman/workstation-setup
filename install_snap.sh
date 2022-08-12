92c3bff2373af56b14a55c0f881cf4ec13ac9053
mari <6347840+maribowman@users.noreply.github.com>
2022-08-12 13:59:44 +0200

    add zoom to app list

--- install_snap.sh
+++ install_snap.sh
@@ -4,6 +4,6 @@ sudo snap refresh
 sudo snap install goland --classic
 sudo snap install helm --classic
 sudo snap install kubectl --classic
-sudo snap install postman keepassxc notion-snap discord teams
+sudo snap install postman keepassxc notion-snap discord teams zoom-client
 
 #sudo snap install tradingview

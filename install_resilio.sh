1a5a0777c70b28adabde75657fc70d2f3e7c9f77
mari__ <bowman.marius@gmail.com>
2022-08-13 13:14:55 +0200

    change resilio installation

--- install_resilio.sh
+++ install_resilio.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# official documentation https://www.resilio.com/blog/official-linux-packages-for-sync-now-available
-deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free
-wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
+VERSION="2.7.3.1381"
+FILENAME="resilio-sync_$VERSION-1_amd64.deb"
+URL="https://download-cdn.resilio.com/$VERSION/Debian/$FILENAME"
+DOWNLOADS="$HOME/Downloads/"
 
-sudo apt update && sudo apt install -y resilio-sync
+(cd "$DOWNLOADS" && curl -OL $URL) && sudo dpkg -i "$DOWNLOADS/resilio-sync_2.7.3.1381-1_amd64.deb"
+
 sudo systemctl enable resilio-sync
+
+sudo usermod -aG "$(id -gn)" rslsync
+sudo usermod -aG rslsync "$USER"
+sudo chmod g+rw "$HOME/Sync"

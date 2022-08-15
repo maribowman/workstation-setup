edfa38a862a5990ef6284bc546bf4737cfa5eba0
mari__ <bowman.marius@gmail.com>
2022-08-15 11:38:31 +0200

    add docker repo to apt cache

--- install_docker.sh
+++ install_docker.sh
@@ -3,7 +3,9 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-sudo apt update && sudo apt install -y docker-ce
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
 
 # execute docker without sudo
 sudo usermod -aG docker "$USER"

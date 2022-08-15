bd9c39bf5d696b720ca6969dac405b93b73fc092
mari__ <bowman.marius@gmail.com>
2022-08-15 11:16:24 +0200

    update docker installation with correct pub key mgmt

--- install_apt.sh
+++ install_apt.sh
@@ -5,4 +5,4 @@ sudo apt update && sudo apt upgrade -y
 # essentials (assuming git is installed to clone this repo)
 sudo apt install -y git-core curl kdiff3 vim zsh snapd
 # docker
-sudo apt install -y apt-transport-https ca-certificates software-properties-common
+sudo apt install -y lsb-release apt-transport-https ca-certificates software-properties-common
--- install_docker.sh
+++ install_docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-apt-cache policy docker-ce
 sudo apt update && sudo apt install -y docker-ce
 
 # execute docker without sudo
 sudo usermod -aG docker "$USER"
 su - "$USER"
+

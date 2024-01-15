0c3e043c4a48e88df63e808658e6702f1d2567cb
mari <bowman.marius@gmail.com>
2024-01-15 18:59:34 +0100

    keep update logs until next run

--- cron/update.sh
+++ cron/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Starting software update in 15s..."
+rm update.log update_copy.log
 sleep 15
 
 echo ${SUDO_PWD} | base64 -d | sudo -S apt update 2>/dev/null | tee update.log
@@ -8,10 +9,12 @@ sudo apt upgrade -y 2>/dev/null | tee -a update.log &
 wait
 
 has_updates=false
+cp update.log update_copy.log
+
 while IFS= read -r line; do
   if $has_updates; then
     has_updates=false
-    sudo apt install -y "$(echo "$line" | xargs echo -n)" &
+    sudo apt install -y "$(echo "$line" | xargs echo -n)" | tee -a update.log &
     wait
     break
   fi
@@ -19,9 +22,8 @@ while IFS= read -r line; do
   if [[ "$line" = *'packages have been kept back'* ]]; then
     has_updates=true
   fi
-done <update.log
+done <update_copy.log
 
-sudo apt autoremove && sudo apt autoclean
-rm update.log
+sudo apt autoremove | tee -a update.log && sudo apt autoclean | tee -a update.log
 
-sudo snap refresh
+sudo snap refresh| tee -a update.log

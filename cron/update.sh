6048c68ad69d38b4db83e69dfb07afcf437520ef
mari <bowman.marius@gmail.com>
2024-01-20 15:30:44 +0100

    fix apt install command

--- cron/update.sh
+++ cron/update.sh
@@ -4,8 +4,9 @@ echo "Starting software update in 15s..."
 rm update.log update_copy.log
 sleep 15
 
-echo ${SUDO_PWD} | base64 -d | sudo -S apt update 2>/dev/null | tee update.log
-sudo apt upgrade -y 2>/dev/null | tee -a update.log &
+echo {SUDO_PWD} | base64 -d | sudo -S apt update 2>&1 | tee update.log &
+wait
+sudo apt upgrade -y 2>&1 | tee -a update.log &
 wait
 
 has_updates=false
@@ -14,7 +15,7 @@ cp update.log update_copy.log
 while IFS= read -r line; do
   if $has_updates; then
     has_updates=false
-    sudo apt install -y "$(echo "$line" | xargs echo -n)" | tee -a update.log &
+    sudo apt install -y $(echo "$line" | xargs echo -n) 2>&1 | tee -a update.log &
     wait
     break
   fi
@@ -23,7 +24,8 @@ while IFS= read -r line; do
     has_updates=true
   fi
 done <update_copy.log
+rm update_copy.log
 
 sudo apt autoremove | tee -a update.log && sudo apt autoclean | tee -a update.log
 
-sudo snap refresh| tee -a update.log
+sudo snap refresh | tee -a update.log

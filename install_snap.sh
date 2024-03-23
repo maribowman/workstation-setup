1b673e132182448a6989c48524fce6a8cebbb9c2
mari <6347840+maribowman@users.noreply.github.com>
2024-03-23 16:12:45 +0100

    update install_snap.sh

--- install_snap.sh
+++ install_snap.sh
@@ -2,8 +2,10 @@
 
 sudo snap refresh
 sudo snap install goland --classic
-sudo snap install helm --classic
+sudo snap install pycharm-professional --classic
 sudo snap install kubectl --classic
-sudo snap install httpie keepassxc notion-snap discord teams zoom-client
+sudo snap install helm --classic
+sudo snap install obsidian --classic
+sudo snap install httpie k9s keepassxc discord teams tradingview zoom-client
 
 #sudo snap install tradingview

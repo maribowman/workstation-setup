5ba549461858b771fc1b65d703a82d9bcca5dd2c
mari <bowman.marius@gmail.com>
2024-12-04 19:41:46 +0100

    add neovim and remove pycharm

--- install_snap.sh
+++ install_snap.sh
@@ -2,10 +2,8 @@
 
 sudo snap refresh
 sudo snap install goland --classic
-sudo snap install pycharm-professional --classic
 sudo snap install kubectl --classic
 sudo snap install helm --classic
 sudo snap install obsidian --classic
-sudo snap install httpie k9s keepassxc discord teams tradingview zoom-client
-
-#sudo snap install tradingview
+sudo snap install nvim --classic
+sudo snap install httpie k9s keepassxc discord tradingview

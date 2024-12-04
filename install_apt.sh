d2296e00b444e1fb1fb8fc6b10866d6c4eaba4d1
mari <bowman.marius@gmail.com>
2024-12-04 19:42:07 +0100

    move apt installs for docker to docker script

--- install_apt.sh
+++ install_apt.sh
@@ -4,5 +4,3 @@ sudo apt update && sudo apt upgrade -y
 
 # essentials (assuming git is installed to clone this repo)
 sudo apt install -y git-core curl kdiff3 vim zsh snapd cifs-utils
-# docker
-sudo apt install -y lsb-release apt-transport-https ca-certificates software-properties-common

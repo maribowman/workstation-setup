7ae299449e94e6a7a09576d53a0e187821d400b6
mari__ <bowman.marius@gmail.com>
2025-04-16 19:24:40 +0200

    updated borg sync

--- borg-backup.sh
+++ borg-backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-REPO="borg@192.168.0.1:/volume1/backups/ubuntu_backup_home/borg"
+REPO="mari@192.168.0.1:/volume1/backups/ubuntu_backup_home/borg"
 BORG_PASSPHRASE=""
 BORG_RSH="ssh"
 BACKUP_SOURCE="/home/$USER/"
-ARCHIVE_NAME="{hostname}-{now:%Y-%m-%d_%H-%M-%S}"
+ARCHIVE_NAME="{hostname}-{now:%Y%m%d_%H%M}"
 EXCLUDE_LIST=(
     "$HOME/.cache"
     "$HOME/.thumbnails"
@@ -15,7 +15,7 @@ export BORG_PASSPHRASE="$BORG_PASSPHRASE"
 
 echo "Starting backup of $BACKUP_SOURCE to $REPO as archive $ARCHIVE_NAME"
 borg create --verbose --stats --compression lz4 --exclude-from <(printf '%s\n' "${EXCLUDE_LIST[@]}") "$REPO::$ARCHIVE_NAME" "$BACKUP_SOURCE"
-borg prune --list --keep-daily=3 --keep-weekly=3 --keep-monthly=3 "$REPO"
+borg prune --list --keep-daily=5 --keep-monthly=3 --keep-yearly=1 "$REPO"
 borg check --verify-data "$REPO"
 
 exit 0

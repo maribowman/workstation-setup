5c0db70be9834af6b6a2e49287acaf500bee4ca7
mari__ <bowman.marius@gmail.com>
2025-06-08 16:24:21 +0200

    update borg backup script

--- borg-backup.sh
+++ borg-backup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-REPO="mari@192.168.0.1:/volume1/backups/ubuntu_backup_home/borg"
+# INITIAL REPO SETUP
+# borg init --encryption=none $PATH
+# borg info --remote-path=/usr/local/bin/borg $USER@$HOST:$PATH
+
+USER="mari"
+HOST="192.168.0.3"
+PATH="/volume1/backups/ubuntu_backup_home/borg"
+REPO="$USER@$HOST:$PATH"
+
 BORG_PASSPHRASE=""
 BORG_RSH="ssh"
 BACKUP_SOURCE="/home/$USER/"
@@ -13,10 +21,12 @@ EXCLUDE_LIST=(
 
 export BORG_PASSPHRASE="$BORG_PASSPHRASE"
 
-echo "Starting backup of $BACKUP_SOURCE to $REPO as archive $ARCHIVE_NAME"
-borg create --verbose --stats --compression lz4 --exclude-from <(printf '%s\n' "${EXCLUDE_LIST[@]}") "$REPO::$ARCHIVE_NAME" "$BACKUP_SOURCE"
-borg prune --list --keep-daily=5 --keep-monthly=3 --keep-yearly=1 "$REPO"
-borg check --verify-data "$REPO"
+echo "Starting backup of $BACKUP_SOURCE to $REPO"
+/usr/bin/borg create --remote-path=/usr/local/bin/borg --verbose --stats --compression lz4 --exclude-from <(printf '%s\n' "${EXCLUDE_LIST[@]}") "$REPO::$ARCHIVE_NAME" "$BACKUP_SOURCE"
+/usr/bin/borg prune --remote-path=/usr/local/bin/borg --list --keep-daily=5 --keep-monthly=3 --keep-yearly=1 "$REPO"
+
+echo "Backup completed! Checking repository consistency"
+/usr/bin/borg check --remote-path=/usr/local/bin/borg --verify-data "$REPO"
 
 exit 0
 
